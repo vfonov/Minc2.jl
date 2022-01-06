eb2a86caee1c4105f7a57f41a17a889f1eb9a331
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-01-06 09:49:34 -0500

    WIP: making initial implementation

--- src/Minc2.jl
+++ src/Minc2.jl
@@ -1,20 +1,33 @@
 module Minc2
 
-    using CBinding
-    using LIBMINC_jll
-    # Main.SYSROOT...,
-	c`$([
-		
-		
-		"-I$(LIBMINC_jll.artifact_dir)/include",
-		"-L$(dirname(LIBMINC_jll.libminc2_path))", 
-        "-L$(dirname(LIBMINC_jll.libminc2_simple_path))", 
-        "-llibminc2-simple","-llibminc2",
-	])`
+    module minc2_simple
+        using LIBMINC_jll
+        using CBinding
+        # Main.SYSROOT...,
+        c`$([
+            "-I$(LIBMINC_jll.artifact_dir)/include",
+            "-L$(dirname(LIBMINC_jll.libminc2_path))", 
+            "-L$(dirname(LIBMINC_jll.libminc2_simple_path))", 
+            "-llibminc2-simple","-llibminc2",
+        ])`
 
-    c"""
-    #include <minc2-simple.h>
-    """ji
+        c"""
+        #include <minc2-simple.h>
+        """ji
+    end
+
+    using CBinding
+    using .minc2_simple
+
+    struct Volume
+        x::Ref
+        function Volume()
+          ret = new(Ref(minc2_allocate0()))
+          finalizer(x -> minc2_destroy(x[]), ret.x)
+          return ret
+        end
+    end
+    
 
 
 end # module
