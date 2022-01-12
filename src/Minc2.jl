33a4c021d1f6f68c31ac81053aea53ef4d8ecbd6
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-01-12 15:41:13 -0500

    WIP: making minimally working example

--- src/Minc2.jl
+++ src/Minc2.jl
@@ -19,15 +19,41 @@ module Minc2
     using CBinding
     using .minc2_simple
 
-    struct Volume
+    struct VolumeHandle
         x::Ref
-        function Volume()
-          ret = new(Ref(minc2_allocate0()))
-          finalizer(x -> minc2_destroy(x[]), ret.x)
+        function VolumeHandle()
+          ret = new(Ref(c"minc2_simple.minc2_allocate0"()))
+          finalizer(x -> c"minc2_simple.minc2_destroy"(x[]), ret.x)
           return ret
         end
     end
+
+    mutable struct minc_header
+        start::Vector{Float64,1}
+        step::Vector{Float64}
+        dir_cos::Array{Float64,2}
+        # history?
+        # tags?
+    end
+
+    function open_minc_file(fname::String)
+        h::VolumeHandle
+        c"minc2_simple.minc2_open"(fname,h.x)
+        return h
+    end
+
+    function close_minc_file(h::VolumeHandle)
+        c"minc2_simple.minc2_close"(h.x)
+    end
+
+    function setup_standard_order(h::VolumeHandle)
+        c"minc2_simple.setup_standard_order"(h.x)
+    end
     
+    function read_minc_volume(h::VolumeHandle)
+        dims=c"minc2_simple.minc2_dimension"()
+        lib.minc2_get_representation_dimensions(h.x,dims)
+    end
 
 
 end # module
