b31a20912af24b3421973bab7b978d87a0c18d3b
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-01-13 01:25:16 -0500

    WIP: minimally working example

--- src/Minc2.jl
+++ src/Minc2.jl
@@ -21,43 +21,43 @@ module Minc2
     using LinearAlgebra
 
 
-    # @enum DIM begin 
-    #     DIM_UNKNOWN=minc2_simple.MINC2_DIM_UNKNOWN 
-    #     DIM_X = minc2_simple.MINC2_DIM_X  
-    #     DIM_Y = minc2_simple.MINC2_DIM_Y 
-    #     DIM_Z = minc2_simple.MINC2_DIM_Z 
-    #     DIM_TIME = minc2_simple.MINC2_DIM_TIME 
-    #     DIM_VEC  = minc2_simple.MINC2_DIM_VEC 
-    #     DIM_END  = minc2_simple.MINC2_DIM_END
-    # end
+    @enum DIM begin
+        DIM_UNKNOWN = Cint(minc2_simple.MINC2_DIM_UNKNOWN)
+        DIM_X = Cint(minc2_simple.MINC2_DIM_X  )
+        DIM_Y = Cint(minc2_simple.MINC2_DIM_Y )
+        DIM_Z = Cint(minc2_simple.MINC2_DIM_Z )
+        DIM_TIME = Cint(minc2_simple.MINC2_DIM_TIME )
+        DIM_VEC  = Cint(minc2_simple.MINC2_DIM_VEC )
+        DIM_END  = Cint(minc2_simple.MINC2_DIM_END)
+    end
 
-    # @enum TYPE begin
-    #     #: minc2 data types
-    #    BYTE     = minc2_simple.MINC2_BYTE 
-    #    SHORT    = minc2_simple.MINC2_SHORT
-    #    INT      = minc2_simple.MINC2_INT 
-    #    FLOAT    = minc2_simple.MINC2_FLOAT 
-    #    DOUBLE   = minc2_simple.MINC2_DOUBLE 
-    #    STRING   = minc2_simple.MINC2_STRING 
-    #    UBYTE    = minc2_simple.MINC2_UBYTE 
-    #    USHORT   = minc2_simple.MINC2_USHORT 
-    #    UINT     = minc2_simple.MINC2_UINT 
-    #    SCOMPLEX = minc2_simple.MINC2_SCOMPLEX 
-    #    ICOMPLEX = minc2_simple.MINC2_ICOMPLEX 
-    #    FCOMPLEX = minc2_simple.MINC2_FCOMPLEX 
-    #    DCOMPLEX = minc2_simple.MINC2_DCOMPLEX 
-    #    MAX_TYPE_ID = minc2_simple.MINC2_MAX_TYPE_ID
-    #    UNKNOWN  = minc2_simple.UNKNOWN
-    # end
+    @enum TYPE begin
+        #: minc2 data types
+       BYTE     = Cint(minc2_simple.MINC2_BYTE )
+       SHORT    = Cint(minc2_simple.MINC2_SHORT)
+       INT      = Cint(minc2_simple.MINC2_INT )
+       FLOAT    = Cint(minc2_simple.MINC2_FLOAT )
+       DOUBLE   = Cint(minc2_simple.MINC2_DOUBLE )
+       STRING   = Cint(minc2_simple.MINC2_STRING )
+       UBYTE    = Cint(minc2_simple.MINC2_UBYTE )
+       USHORT   = Cint(minc2_simple.MINC2_USHORT )
+       UINT     = Cint(minc2_simple.MINC2_UINT )
+       SCOMPLEX = Cint(minc2_simple.MINC2_SCOMPLEX )
+       ICOMPLEX = Cint(minc2_simple.MINC2_ICOMPLEX )
+       FCOMPLEX = Cint(minc2_simple.MINC2_FCOMPLEX )
+       DCOMPLEX = Cint(minc2_simple.MINC2_DCOMPLEX )
+       MAX_TYPE_ID = Cint(minc2_simple.MINC2_MAX_TYPE_ID)
+       UNKNOWN  = Cint(minc2_simple.MINC2_UNKNOWN)
+    end
 
-    # @enum STATUS begin
-    #     # minc2 status
-    #     SUCCESS  = minc2_simple.MINC2_SUCCESS,
-    #     ERROR    = minc2_simple.MINC2_ERROR
-    # end
+    @enum STATUS begin
+        # minc2 status
+        SUCCESS  = Cint(minc2_simple.MINC2_SUCCESS)
+        ERROR    = Cint(minc2_simple.MINC2_ERROR)
+    end
 
     macro minc2_check( ex )
-        return :($(esc(ex)) == 0 ? $(nothing) : throw(SystemError("MINC2 error in ")))
+        return :($(esc(ex)) == 0 ? $(nothing) : throw(SystemError("MINC2 error in " * __module__ * " " *__source__)))
     end
 
     mutable struct VolumeHandle
@@ -71,12 +71,13 @@ module Minc2
     end
 
     mutable struct MincHeader
+        dims::Vector{Int32}
         start::Vector{Float64}
         step::Vector{Float64}
         dir_cos::Matrix{Float64}
 
         function MincHeader(ndim)
-            new(zeros(ndim),ones(ndim),Matrix{Float64}(I, 3, 3))
+            new(zeros(ndim),zeros(ndim),ones(ndim),Matrix{Float64}(I, 3, 3))
         end
     end
 
@@ -92,21 +93,24 @@ module Minc2
     end
 
     function ndim(h::VolumeHandle)
-        dd=Ref{Int}(0)
+        dd = Ref{Int}(0)
         @minc2_check minc2_simple.minc2_ndim( h.x[], dd )
         return dd[]
     end
 
     function setup_standard_order(h::VolumeHandle)
-        @minc2_check minc2_simple.setup_standard_order(h.x[])
+        @minc2_check minc2_simple.minc2_setup_standard_order(h.x[])
     end
     
     function header(h::VolumeHandle)
         hdr = MincHeader(ndim(h))
         dd = Ref{c"minc2_simple.struct minc2_dimension *"}()
 
+        # this is supposed to be in a standard order
         @minc2_check minc2_simple.minc2_get_representation_dimensions(h.x[], dd)
-        for i=1:length(hdr.start)
+
+        for i = 1:length(hdr.start)
+            hdr.dims[i] = dd[][i].length
             hdr.start[i] = dd[][i].start
             hdr.step[i] = dd[][i].step
             if dd[][i].have_dir_cos != 0
@@ -117,14 +121,13 @@ module Minc2
         return hdr
     end
 
+
     function read_minc_volume(h::VolumeHandle)
-        dims=c"minc2_simple.minc2_dimension"()
-        hdr=header(h)
-
-        @minc2_check minc2_simple.minc2_get_representation_dimensions(h.x[], dims)
-        ndims::Integer=ndim(h)
-
-        return ndims
+        setup_standard_order( h )
+        hdr = header( h )
+        volume = Array{Float32}(undef,hdr.dims...)
+        @minc2_check minc2_simple.minc2_load_complete_volume(h.x[], Base.unsafe_convert(Ptr{Cvoid},volume), minc2_simple.MINC2_FLOAT )
+        return hdr,volume
     end
 
 
