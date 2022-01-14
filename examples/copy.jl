025d4589d55a47d4f751561318fde2578126d690
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-01-14 15:21:24 -0500

    WIP: improving functionality of minc reader/writer

--- examples/copy.jl
+++ examples/copy.jl
@@ -1,14 +1,33 @@
 using Minc2 # for reading MINC2 files
+using ArgParse
 
 
-a = Minc2.open_minc_file("/home/vfonov/mni/icbm152_model_09c/mni_icbm152_t1_tal_nlin_asym_09c.mnc")
+function parse_commandline()
+    s = ArgParseSettings()
+    @add_arg_table s begin
+        "in"
+            help = "Input minc file"
+            required = true
+        "out"
+            help = "Output file"
+            required = true
+    end
+    parse_args(ARGS, s)
+end
+
+args = parse_commandline()
+
+a = Minc2.open_minc_file(args["in"])
 mri, hdr, store_hdr=Minc2.read_minc_volume(a,Float32)
+history = Minc2.read_minc_history(a)
 Minc2.close_minc_file(a)
 
+println("History:", history)
 println("store_hdr:",store_hdr)
 println("hdr:",hdr)
 
 b = Minc2.define_minc_file(store_hdr, UInt16, Float32)
-Minc2.create_minc_file(b,"test.mnc")
+Minc2.create_minc_file(b,args["out"])
+Minc2.write_minc_history(b,"from Julia")
 Minc2.write_minc_volume(b,mri)
 Minc2.close_minc_file(b)
--- src/Minc2.jl
+++ src/Minc2.jl
@@ -20,7 +20,6 @@ module Minc2
 
     using CBinding
     using .minc2_simple
-    using LinearAlgebra
 
 
     @enum DIM begin
@@ -32,6 +31,11 @@ module Minc2
         DIM_VEC  = Cint(minc2_simple.MINC2_DIM_VEC )
         DIM_END  = Cint(minc2_simple.MINC2_DIM_END)
     end
+
+    # TODO: double check?
+    minc2_spatial=Dict(Cint(minc2_simple.MINC2_DIM_X)=>1,
+                       Cint(minc2_simple.MINC2_DIM_Y)=>2,
+                       Cint(minc2_simple.MINC2_DIM_Z)=>3)
 
     # map Julia types to minc2 data types
     julia_to_minc2 = Dict(
@@ -77,31 +81,45 @@ module Minc2
         end
     end
 
+    """
+    Structure describing spatial orientation of the minc file
+    """
     mutable struct MincHeader
         dims::Vector{Int32}
         start::Vector{Float64}
         step::Vector{Float64}
         dir_cos::Matrix{Float64}
-
+        dir_cos_valid::Vector{Bool}
         # TODO: figure out how to make it compatible with other libraries
         axis::Vector{Int32}
 
         function MincHeader(ndim)
-            new(zeros(ndim), zeros(ndim), ones(ndim), Matrix{Float64}(I, 3, 3), zeros(ndim))
+            new(zeros(ndim),
+                zeros(ndim), 
+                ones(ndim), 
+                Matrix{Float64}(undef, ndim, 3),
+                zeros(Bool,ndim),
+                zeros(ndim))
         end
     end
 
+    """
+    Open minc file, return handle
+    """
     function open_minc_file(fname::String)
         h = VolumeHandle()
         @minc2_check minc2_simple.minc2_open(h.x[], fname)
         return h
     end
 
-    function define_minc_file(hdr::MincHeader,::Type{A}=Float32,::Type{B}=A) where {A,B}
+    """
+    Define new minc file structure, return handle
+    """
+    function define_minc_file(hdr::MincHeader,::Type{Store}=Float32,::Type{Repr}=Store) where {Store,Repr}
         h = VolumeHandle()
 
-        _store_type = julia_to_minc2[Type{A}]
-        _representation_type = julia_to_minc2[Type{B}]
+        _store_type = julia_to_minc2[Type{Store}]
+        _representation_type = julia_to_minc2[Type{Repr}]
         
         _dims = [
             c"minc2_simple.struct minc2_dimension"(
@@ -109,8 +127,8 @@ module Minc2
                     length=j,
                     start=hdr.start[i],
                     step=hdr.step[i],
-                    have_dir_cos=0,
-                    dir_cos=Tuple(hdr.dir_cos)
+                    have_dir_cos=Int(hdr.dir_cos_valid[i]),
+                    dir_cos=Tuple(hdr.dir_cos[i,:])
                 ) for (i,j) in enumerate(hdr.dims)
         ]
         push!(_dims,c"minc2_simple.struct minc2_dimension"(id=Cint(minc2_simple.MINC2_DIM_END)))
@@ -129,24 +147,59 @@ module Minc2
         return h
     end
 
+    """
+    Create empty minc file on disk, structure need to be defined in advance
+    """
     function create_minc_file(h::VolumeHandle, path::String)
         @minc2_check minc2_simple.minc2_create(h.x[], path )
     end
 
+    """
+    Close currently open minc file, commit data on disk
+    """
     function close_minc_file(h::VolumeHandle)
         @minc2_check minc2_simple.minc2_close( h.x[] )
     end
 
+    """
+    Query number of dimensions in the minc file
+    """
     function ndim(h::VolumeHandle)
         dd = Ref{Int}(0)
         @minc2_check minc2_simple.minc2_ndim( h.x[], dd )
         return dd[]
     end
 
+    """
+    Prepare to read volume in standard order: [V,X,Y,Z,TIME]
+    """
     function setup_standard_order(h::VolumeHandle)
         @minc2_check minc2_simple.minc2_setup_standard_order(h.x[])
     end
     
+    function _hdr_convert!(hdr,dd)
+        for i = 1:length(hdr.start)
+            hdr.dims[i] = dd[][i].length
+            hdr.start[i] = dd[][i].start
+            hdr.step[i] = dd[][i].step
+            if dd[][i].have_dir_cos != 0
+                hdr.dir_cos[i,:] .= dd[][i].dir_cos[:]
+                hdr.dir_cos_valid[i] = true
+            else # TODO: do it only for spatial dimension
+                hdr.dir_cos[i,:] = zeros(3)
+                if haskey(minc2_spatial, dd[][i].id)  
+                    hdr.dir_cos[i,minc2_spatial[dd[][i].id]] = 1.0
+                end
+                hdr.dir_cos_valid[i] = false
+            end
+            hdr.axis[i] = dd[][i].id
+        end
+        return hdr
+    end
+
+    """
+    Return volume representation header
+    """
     function representation_header(h::VolumeHandle)::MincHeader
         hdr = MincHeader(ndim(h))
         dd = Ref{c"minc2_simple.struct minc2_dimension *"}()
@@ -154,19 +207,12 @@ module Minc2
         # this is supposed to be in a standard order
         @minc2_check minc2_simple.minc2_get_representation_dimensions(h.x[], dd)
 
-        for i = 1:length(hdr.start)
-            hdr.dims[i] = dd[][i].length
-            hdr.start[i] = dd[][i].start
-            hdr.step[i] = dd[][i].step
-            if dd[][i].have_dir_cos != 0
-                hdr.dir_cos[i,:] .= dd[][i].dir_cos[:]
-            hdr.axis[i] = dd[][i].id
-            end
-        end
-        
-        return hdr
+        return _hdr_convert!(hdr,dd)
     end
 
+    """
+    return volume on-disk stucture header
+    """
     function store_header(h::VolumeHandle)::MincHeader
         hdr = MincHeader(ndim(h))
         dd = Ref{c"minc2_simple.struct minc2_dimension *"}()
@@ -174,19 +220,13 @@ module Minc2
         # this is supposed to be in a standard order
         @minc2_check minc2_simple.minc2_get_store_dimensions(h.x[], dd)
 
-        for i = 1:length(hdr.start)
-            hdr.dims[i] = dd[][i].length
-            hdr.start[i] = dd[][i].start
-            hdr.step[i] = dd[][i].step
-            if dd[][i].have_dir_cos != 0
-                hdr.dir_cos[i,:] .= dd[][i].dir_cos[:]
-            end
-            hdr.axis[i] = dd[][i].id
-        end
-        
-        return hdr
+        return _hdr_convert!(hdr,dd)
     end
 
+    """
+    Read the actual volume using handle
+    return volume, representation header,storage header
+    """
     function read_minc_volume(h::VolumeHandle, ::Type{T}=Float32 ) where {T}
         # TODO: use ImageMetadata to store header contents?
         setup_standard_order( h )
@@ -198,9 +238,150 @@ module Minc2
         return volume, hdr, store_hdr
     end
 
+    """
+    Read the actual volume using path
+    return volume, representation header,storage header
+    """
+    function read_minc_volume(path::String, ::Type{T}=Float32 ) where {T}
+        handle = open_minc_file(path)
+        volume, hdr, store_hdr = read_minc_volume(handle,T)
+        close_minc_file(handle)
+
+        return volume, hdr, store_hdr
+    end
+
+    """
+    write full volume to file, file should be defined and created
+    return nothing
+    """
     function write_minc_volume(h::VolumeHandle, volume::Array{T} ) where {T}
         setup_standard_order( h )
         @minc2_check minc2_simple.minc2_save_complete_volume(h.x[], Base.unsafe_convert(Ptr{Cvoid},volume),julia_to_minc2[Type{T}])
+        return nothing
+    end
+
+    """
+    Copy metadata from one file to another
+    """
+    function copy_minc_metadata(i::VolumeHandle,o::VolumeHandle)
+        @minc2_check minc2_simple.minc2_copy_metadata(i.x[],o.x[])
+    end
+    
+
+    """
+    Read minc2 header attribute
+    :param group: attribute group name
+    :param attribute: attribute name
+    :return:
+    """
+    function read_minc_attribute(h::VolumeHandle, group::String, attribute::String)
+
+        attr_type  = Ref{Int}(0)
+        attr_length = Ref{Int}(0)
+
+        # assume that if we can't get attribute type, it's missing, return nil
+
+        @minc2_check minc2_simple.minc2_get_attribute_type(h.x[], group, attribute, attr_type)
+        @minc2_check minc2_simple.minc2_get_attribute_length(h.x[], group, attribute, attr_length)
+        
+        buf = missing
+
+        if attr_type[] == minc2_simple.MINC2_STRING
+            _buf = Vector{UInt8}(undef,attr_length[])
+            @minc2_check minc2_simple.minc2_read_attribute(h.x[], group, attribute, _buf, attr_length[])
+            buf = String(_buf)
+        else
+            # TODO: finish this
+            throw(SystemError("MINC2 not implemented"))
+            # data_type = attr_type[0]
+            # buf=None
+            # if data_type in minc2_file.__minc2_to_numpy:
+            #     dtype=minc2_file.__minc2_to_numpy[data_type]
+            #     shape=[attr_length[0]]
+            #     buf=np.empty(shape,dtype,'C')
+            # else:
+            #     raise minc2_error("Error determining attribute type {}:{}".format(group,attribute))
+
+            # if lib.minc2_read_attribute(self._v,group,attribute,ffi.cast("void *", buf.ctypes.data),attr_length[0])!=lib.MINC2_SUCCESS:
+            #     raise minc2_error("Error reading attribute {}:{}".format(group,attribute))
+        end
+
+        return buf
+    end
+
+
+    """
+    Store attribute into minc2 file
+    :param group:  group name
+    :param attribute:  attribute name
+    :param value:  attribute value
+    :return:
+    """
+    function write_minc_attribute(h::VolumeHandle, group::String, attribute::String, value::T) where {T}
+        val_type = julia_to_minc2[Type{T}]
+        attr_length = length(value)
+        # TODO: make this work for other type
+        # Right now it might write garbage for anything but strings
+        if isa(value,String)
+            _value=Vector{UInt8}(value)
+            @minc2_check minc2_simple.minc2_write_attribute(h.x[], group, attribute, Base.unsafe_convert(Ptr{Cvoid},_value), attr_length, val_type)
+        else
+            @minc2_check minc2_simple.minc2_write_attribute(h.x[], group, attribute, Base.unsafe_convert(Ptr{Cvoid},value), attr_length, val_type)
+        end
+    end
+
+    """
+    return history string
+    """
+    function read_minc_history(i::VolumeHandle)::Union{String, Missing}
+        return read_minc_attribute(i,"","history")
+    end
+
+    """
+    write history string
+    """
+    function write_minc_history(i::VolumeHandle,history::String)
+        write_minc_attribute(i,"","history",history)
+    end
+
+
+    """
+    write full volume to file, need to provide details of file structure
+    return nothing
+    """
+    function write_minc_volume(path::String, ::Type{Store}, 
+            store_hdr::Union{MincHeader, Nothing}, 
+            volume::Array{Repr};like::Union{String, Nothing}=nothing, 
+            history::Union{String, Nothing}=nothing ) where {Store,Repr}
+        if isdefined(like)
+            # TODO: check if store_hdr is compatible with volume
+
+            handle = define_minc_file(store_hdr, Store, Repr)
+            Minc2.create_minc_file(handle,path)
+
+            if isdefined(history)
+
+            end
+
+            Minc2.write_minc_volume(handle,volume)
+            close_minc_file(handle)
+        else
+            # need to copy file structure
+            in_h = open_minc_file(like)
+            store_hdr = store_header( in_h )
+
+            # TODO: check if store_hdr is compatible with volume
+            handle = define_minc_file(store_hdr, Store, Repr)
+            Minc2.create_minc_file(handle,path)
+            
+            copy_minc_metadata(in_h,handle)
+
+            Minc2.write_minc_volume(handle,volume)
+            close_minc_file(in_h)
+            close_minc_file(handle)
+        end
+
+        return nothing
     end
 
 
