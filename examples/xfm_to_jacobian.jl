ba1d07a37e18ff0b1790ff26e37cb43e81120040
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2022-09-09 14:51:54 -0400

    Added function to calculate the jacobian

--- examples/xfm_to_jacobian.jl
+++ examples/xfm_to_jacobian.jl
@@ -2,29 +2,41 @@ using Minc2 # for reading MINC2 files
 using Interpolations
 using ArgParse
 using StaticArrays
+using LinearAlgebra
 
-function resample_grid_volume(in_vol::Array{T,4}, 
-            out_vol::Array{T,4}, 
-            v2w::Minc2.AffineTransform{C}, 
-            w2v::Minc2.AffineTransform{C}, 
-            itfm::Vector{Minc2.AnyTransform{C,C}};
+function calculate_jacobian(
+            out_vol::Array{T,3}, 
+            v2w::Minc2.AffineTransform{C},
+            tfm::Vector{Minc2.AnyTransform{C,C}};
             interp::I=BSpline(Quadratic(Line(OnCell()))),
             fill=0.0,
             ftol=1.0/80,
             max_iter=10) where {C,T,I}
 
-    # NEED to interpolate only over spatial dimensions
-    in_vol_itp = extrapolate( interpolate( in_vol, (NoInterp(),interp,interp,interp)),fill)
+    # calculate scaling matrix from the voxel to world matrix
+    f = svd(v2w.rot)
+    dir_cos = f.U * f.Vt
+    sc = Base.inv(v2w.rot * Base.inv(dir_cos))
+    @info "Scaling matrix" sc
 
-    @simd for c in CartesianIndices(view(out_vol,1,:,:,:))
+    # First step: generate vector field of transformations
+    vector_field = Array{T}(undef, 3, size(out_vol)...)
+
+    @simd for c in CartesianIndices(out_vol)
         orig = Minc2.transform_point(v2w, c )
-        dst  = Minc2.transform_point(itfm, orig; ftol, max_iter )
-        dst_v= Minc2.transform_point(w2v, dst ) .+ 1.0
+        dst  = Minc2.transform_point(tfm, orig; ftol, max_iter )
         
-        for i in eachindex(view(out_vol,:,1,1,1))
-            @inbounds out_vol[i,c] = in_vol_itp(i, dst_v...)
-        end
+        @inbounds vector_field[:,c] .= dst # .- orig
     end
+
+    # Second step: calculate jacobian determinant 
+    vector_field_itp = extrapolate( interpolate( vector_field, (NoInterp(),interp,interp,interp)), Flat())
+
+    @simd for c in CartesianIndices(out_vol)
+        grad = hcat([ Interpolations.gradient( vector_field_itp, i, Tuple(c)...) for i in 1:3 ]...)
+        out_vol[c] = det(grad'*sc)
+    end
+
     out_vol
 end
 
@@ -33,14 +45,11 @@ function parse_commandline()
     s = ArgParseSettings()
 
     @add_arg_table s begin
-        "in"
-            help = "Input grid minc file"
-            required = true
         "transform"
-            help = "Transform"
+            help = "Input transform file"
             required = true
         "out"
-            help = "Output grid minc file"
+            help = "Output jacobian minc file"
             required = true
         "--like"
             help = "Reference "
@@ -61,46 +70,55 @@ function parse_commandline()
             help = "Fill value"
             arg_type = Float64
             default = 0.0
+        "--invert"
+            help = "Invert transform"
+            action = :store_true
+
     end
     parse_args(ARGS, s)
 end
 
-args = parse_commandline()
+#####
+begin 
+    args = parse_commandline()
+
+    tfm=Minc2.load_transforms(args["transform"])
+
+    if args["invert"]
+        tfm=Minc2.inv(tfm)
+    end
+
+    @info "Input transform" tfm
 
 
-in_vol,in_hdr,in_store_hdr = Minc2.read_minc_volume_std(args["in"], Float64)
 
-#@info "in_vol:",size(in_vol)
-#@info "in_hdr:",in_hdr
+    if !isnothing(args["like"])
+        # search for reference volume in .xfm file
+        out_vol,out_hdr,out_store_hdr = Minc2.empty_like_minc_volume_std(args["like"],Float64)
+    else
+        idx_grid=findfirst(x->(x isa Minc2.GridTransform || x isa Minc2.InverseGridTransform), tfm)
+        if isnothing(idx_grid)
+            @error "Can't find grid to use as reference, provide --like "
+            exit 
+        end
 
-if !isnothing(args["like"])
-    out_vol,out_hdr,out_store_hdr = Minc2.empty_like_minc_volume_std(args["like"],Float64)
-else
-    out_vol = Array{Float64}(undef, size(in_vol)...)
-    out_hdr = in_hdr
-    out_store_hdr = in_store_hdr
+        out_vol = Array{Float64}(undef, size(tfm[idx_grid].vector_field)[2:4]...)
+        out_hdr = Minc2.create_header_from_v2w(size(out_vol), tfm[idx_grid].voxel_to_world,vector_dim=false)
+        out_store_hdr = out_hdr
+    end
+
+    v2w=Minc2.voxel_to_world(out_hdr)
+
+    if args["order"] == 0     # nearest
+        #in_vol_itp = extrapolate( interpolate( in_vol, BSpline(Constant())),args["fill"])
+        calculate_jacobian(out_vol, v2w,tfm; interp=BSpline(Constant()),fill=args["fill"])
+    elseif args["order"] == 1 # linear
+        calculate_jacobian(out_vol, v2w,tfm; interp=BSpline(Linear()),fill=args["fill"])
+    elseif args["order"] == 2 # quadratic
+        calculate_jacobian(out_vol, v2w,tfm; interp=BSpline(Quadratic(Line(OnCell()))),fill=args["fill"])
+    elseif args["order"] == 3 # cubic
+        calculate_jacobian(out_vol, v2w,tfm; interp=BSpline(Cubic(Line(OnCell()))),fill=args["fill"])
+    end
+
+    Minc2.write_minc_volume_std(args["out"], UInt16, out_store_hdr, out_vol)
 end
-
-v2w=Minc2.voxel_to_world(out_hdr)
-w2v=Minc2.world_to_voxel(in_hdr)
-
-tfm=Minc2.load_transforms(args["transform"])
-itfm=Minc2.inv(tfm)
-
-if args["order"] == 0     # nearest
-    #in_vol_itp = extrapolate( interpolate( in_vol, BSpline(Constant())),args["fill"])
-    resample_grid_volume(in_vol, out_vol,v2w,w2v,itfm; interp=BSpline(Constant()),fill=args["fill"])
-elseif args["order"] == 1 # linear
-    @timev resample_grid_volume(in_vol,out_vol,v2w,w2v,itfm; interp=BSpline(Linear()),fill=args["fill"])
-    #in_vol_itp = extrapolate( interpolate( in_vol, BSpline(Linear())),args["fill"])
-elseif args["order"] == 2 # quadratic
-    @timev resample_grid_volume(in_vol,out_vol,v2w,w2v,itfm; interp=BSpline(Quadratic(Line(OnCell()))),fill=args["fill"])
-    #statprofilehtml()
-    #in_vol_itp = extrapolate( interpolate( in_vol, BSpline(Quadratic(Line(OnCell())))), args["fill"])
-elseif args["order"] == 3 # cubic
-    resample_grid_volume(in_vol,out_vol,v2w,w2v,itfm; interp=BSpline(Cubic(Line(OnCell()))),fill=args["fill"])
-    #in_vol_itp = extrapolate( interpolate( in_vol, BSpline(Cubic(Line(OnCell())))), args["fill"])
-end
-
-
-Minc2.write_minc_volume_std(args["out"], UInt16, out_store_hdr, out_vol)
