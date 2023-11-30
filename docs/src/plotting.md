eae872fcc90bbd19a7003aeaf645ad10da39b4ab
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2023-11-29 23:44:35 -0500

    Added example output

--- docs/src/plotting.md
+++ docs/src/plotting.md
@@ -5,7 +5,6 @@ CurrentModule = Minc2
 
 These are helper functions for plotting `Volume3D` objects using Makie. To make them available one must first load `Makie` or equivalent (i.e `CairoMakie`)
 
-
 ### Show MNI-ICBM152 template contours with tissue masks overlays
 
 ```julia
@@ -31,13 +30,11 @@ save("mni_icbm152_segmentation.png", fig, px_per_unit = 1)
 ```
 
 Will produce
-![MNI-ICBM152](https://github.com/vfonov/Minc2.jl/blob/main/docs/src/assets/mni_icbm152_segmentation.png?raw=true)
-
+![MNI-ICBM152 Segmentation](https://github.com/vfonov/Minc2.jl/blob/main/docs/src/assets/mni_icbm152_segmentation.png?raw=true)
 
 ```@docs
 Minc2.draw_outline_with_labels
 ```
-
 
 ### Show MNI-ICBM152 template contours with GM proability map
 
@@ -66,7 +63,7 @@ save("mni_icbm152_gm.png", fig, px_per_unit = 1)
 ```
 
 Will produce
-![MNI-ICBM152](https://github.com/vfonov/Minc2.jl/blob/main/docs/src/assets/mni_icbm152_gm.png?raw=true)
+![MNI-ICBM152 GM](https://github.com/vfonov/Minc2.jl/blob/main/docs/src/assets/mni_icbm152_gm.png?raw=true)
 
 ```@docs
 Minc2.draw_outline_with_heatmap
@@ -74,7 +71,48 @@ Minc2.draw_outline_with_heatmap
 
 ### Show two-tailed t-statistics on DBM style analysis
 
+
+
 ```julia
-# TODO
+using CairoMakie
+using Minc2
+
+# read T1w scan and crop black area around ROI
+anat=Minc2.crop_volume(
+    Minc2.read_volume("mni_icbm152_t1_tal_nlin_sym_09c.mnc",store=Float64),
+    [[15,15],[20,20],[20,20]])
+
+# Read brain ROI, should be the same sampling as anat
+mask=Minc2.crop_volume(
+    Minc2.read_volume("mni_icbm152_t1_tal_nlin_sym_09c_mask.mnc",store=UInt8),
+    [[15,15],[20,20],[20,20]])
+
+# read statistics map, does not have to be the same resolution as anat
+vol_t=Minc2.read_volume("t_AD_vs_CN_2mm.mnc",store=Float64)
+
+# statistical limits (two-tailed)
+stat_limits  = [2.9 , 5]
+
+# start new figure
+fig = Figure()
+
+g1  = f[1,1] = GridLayout()
+Label(f[1,1,Top()],"AD vs CN-")
+
+draw_outline_with_heatmap_symmetric(g1, anat, mask, vol_stat;
+    heat_limits=stat_limits,
+    show_colorbar=true,
+    cmap_pos=:YlOrRd_9,cmap_neg=:YlGnBu_9,
+    nslices=5,
+    colorbar_label="t-statistics")
+
+resize_to_layout!(fig)
+
+save("t-statistics.png", fig, px_per_unit = 1) 
 ```
 
+![T-statistics](https://github.com/vfonov/Minc2.jl/blob/main/docs/src/assets/t-statistics.png?raw=true)
+
+```@docs
+Minc2.draw_outline_with_heatmap_symmetric
+```
--- src/minc_makie.jl
+++ src/minc_makie.jl
@@ -162,3 +162,149 @@ function draw_outline_with_heatmap(
     Makie.rowgap!(gc, 0)
     return gc
 end
+
+"""
+    draw_outline_with_heatmap_symmetric(
+        layout, anat, mask, heat; 
+        cmap_pos=:YlOrRd_9,
+        cmap_neg=:YlGnBu_9,
+        levels=[20, 40],
+        heat_limits=nothing,
+        show_colorbar=true,
+        under=nothing, 
+        over=nothing,
+        nslices=4,
+        alpha=0.7,
+        colorbar_label="",
+        slices_gap=5,
+        colorbar_gap=10)
+
+Draws a two-sided (symmetric positive and negative) heatmap of `heat` on top of `anat` in a grid layout `layout`.
+
+* `layout` is a grid layout
+* `anat` is a Volume3D with anatomical image
+* `mask` is a Volume3D with ROI
+* `heat` is a Volume3D with overlay heatmap, can be arbitrary sampling
+* `heat_limits` limits for the heatmap
+* `cmap_pos` is a color map to use for positive values, if nothing then don't show positive values
+* `cmap_neg` is a color map to use for negative values, if nothing then don't show negative values
+* `levels` is a list of levels of `anat` to use for contours
+* `show_colorbar` is a boolean flag to show colorbar
+* `colorbar_label` is a label for the colorbar
+* `nslices` is a number of slices to show
+"""
+function draw_outline_with_heatmap_symmetric(
+    layout, anat, mask, heat; 
+    cmap_pos=:YlOrRd_9,
+    cmap_neg=:YlGnBu_9,
+    levels=[20, 40],
+    heat_limits=nothing,
+    show_colorbar=true,
+    under=nothing, 
+    over=nothing,
+    nslices=4,
+    alpha=0.7,
+    colorbar_label="",
+    slices_gap=5,
+    colorbar_gap=10)
+
+    sz = size(Minc2.array(anat))
+    w = max(sz...)
+
+    if !isnothing(cmap_neg)
+        cmap_neg_rev = Reverse(cmap_neg)
+    end
+
+    # resample heat to match sampling of mask
+    heat_r = Minc2.empty_volume_like(mask; store=Float64)
+    Minc2.resample_volume!(heat, heat_r)
+
+    if isnothing(heat_limits)
+        vol_sel = Minc2.array(heat_r)[Minc2.array(mask)>0]
+        vol_pos = vol_sel[vol_sel .> 0]
+        vol_neg = - vol_sel[vol_sel .< 0]
+        heat_limits = extrema(vcat(vol_pos, vol_neg))
+    end
+    
+    # for showing ROIs
+    opts_pos=(colorrange = heat_limits, 
+        colormap=cmap_pos,
+        interpolate=false, lowclip=nothing, highclip=over, 
+        alpha, transparency=true)
+
+    opts_neg=(colorrange = (-heat_limits[2],-heat_limits[1]), 
+        colormap=cmap_neg_rev,
+        interpolate=false, highclip=nothing, lowclip=under, 
+        alpha, transparency=true)
+
+    opt_c=(levels=levels, color=:black, alpha=0.7, transparency=true)
+
+    # width=w, height=w, 
+    aopt=(aspect=Makie.DataAspect(), 
+        titlevisible =false, topspinevisible=false,yticksvisible = false,
+        yticklabelsvisible = false, bottomspinevisible=false, leftspinevisible=false, 
+        rightspinevisible = false, xgridvisible=false,ygridvisible=false)
+
+    gc = layout
+    baxs = []
+
+    # show brain with ROIs
+   
+    for ax in 1:3, s in 1:nslices
+        heat_pos = deepcopy(selectdim(Minc2.array(heat_r), ax, fld1(sz[ax]*s,nslices+1)))
+        heat_neg = deepcopy(heat_pos)
+
+        # make transparent
+        heat_pos[heat_pos .<  heat_limits[1]] .= NaN
+        heat_neg[heat_pos .> -heat_limits[1]] .= NaN
+
+        mask_s = selectdim(Minc2.array(mask), ax, fld1(sz[ax]*s,nslices+1))
+        anat_s = selectdim(Minc2.array(anat), ax, fld1(sz[ax]*s,nslices+1))
+
+        # make background transparent
+        heat_pos[mask_s .== 0] .= NaN
+        heat_neg[mask_s .== 0] .= NaN
+
+        (w,h)  = size(anat_s)
+        b1a = Makie.Axis(gc[ax,s]; width=w, height=h, aopt...)
+        Makie.hidedecorations!(b1a, grid = false)
+
+        push!(baxs,b1a)
+
+        if !isnothing(cmap_pos)
+            Makie.heatmap!(b1a, heat_pos;opts_pos...)
+        end
+
+        if !isnothing(cmap_neg)
+            Makie.heatmap!(b1a, heat_neg;opts_neg...)
+        end
+        Makie.contour!(b1a, anat_s;opt_c...)
+    end
+
+    Makie.tightlimits!.(baxs)
+
+    # add color bars
+    if show_colorbar
+        gb = gc[:,nslices+1] = GridLayout(default_rowgap=colorbar_gap, default_colgap=0)
+        # separate from the main heatmaps
+        if !isnothing(cmap_pos)
+            Makie.Colorbar( gb[1,1], colormap=cmap_pos, 
+                    limits = heat_limits, 
+                    highclip = over, 
+                    lowclip = nothing)
+        end
+        if !isnothing(cmap_neg)
+            Makie.Colorbar( gb[2,1], colormap=cmap_neg_rev, 
+                    limits = (-heat_limits[2],-heat_limits[1]), 
+                    highclip = nothing, lowclip = under)
+        end
+        Makie.Label(gb[:,2], colorbar_label , rotation = pi/2)
+    end
+    Makie.colgap!(gc, slices_gap)
+    Makie.rowgap!(gc, slices_gap)
+
+    if show_colorbar
+        Makie.colgap!(gc, nslices, colorbar_gap)
+    end
+    return gc
+end
